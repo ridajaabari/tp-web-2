4a3f04c1efc2f19882900fcefbabf1fcd594f2e0
ridajaabari <ridajaabari.18@gmail.com>
2025-02-26 18:18:24 +0100

    This is my web app

---
+++ script.js
@@ -0,0 +1,5 @@
+let nom ="Rida";
+let age = 20;
+let ville ="marrakech";
+console.log ("je m'appelle",nom ,"j'ai ",age ,"j'habite a ",ville);
+console .log (`je m'appelle ${nom }j'ai ${age} j'habite a ${ville}`);
